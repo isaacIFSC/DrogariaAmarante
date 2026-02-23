1f6ee5d469c686952387a1af6555046ad298b5a3
isaacIFSC <isaac.c17@aluno.ifsc.edu.br>
2026-02-23 19:55:33 -0300

    Project structure

---
+++ drugstoreAmarante/src/main/java/com/example/drugstoreAmarante/DrugstoreAmaranteApplication.java
@@ -0,0 +1,13 @@
+package com.example.drugstoreAmarante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugstoreAmaranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugstoreAmaranteApplication.class, args);
+	}
+
+}
